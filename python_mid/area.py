b1678cb41e700cf79121b8c582c2d1c59f4abd7b
Sanghyuk <saanghyuk@gmail.com>
2021-04-06 23:56:13 +0900

    20210406 - 2

---
+++ python_mid/area.py
@@ -0,0 +1,9 @@
+
+PI = 3.14
+
+def circle(radius):
+    return PI * radius * radius
+
+def square(length):
+    return length*length
+
---
+++ python_mid/run.py
@@ -0,0 +1,7 @@
+from area import circle, square
+
+def square(length):
+    return 4*length
+
+
+print(square(3))
